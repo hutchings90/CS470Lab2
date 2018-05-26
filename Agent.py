895d98e844021bbdec6f19fad089feb6e00da04a
Jeffrey Hutchings - school <hutchings90@gmail.com>
2018-05-26 16:27:27 -0600

    Results for Problem 1

--- Agent.py
+++ Agent.py
@@ -1,12 +1,15 @@
 import random
 
 class Agent:
-    likelihoods = [[1 / 3, 1 / 3, 1 / 3], [1 / 3, 1 / 3, 1 / 3], [1 / 3, 1 / 3, 1 / 3]]
-    utilities = [100, -70, -70]
+    likelihoods = [[1 / 2, 1 / 4, 1 / 4], [1 / 4, 1 / 2, 1 / 4], [1 / 4, 1 / 4, 1 / 2]]
+    utilities = [30, 30, 30]
 
     def __init__(self, i):
         self.id = i
-        self.reset()
+        self.priors = [1 / 3] * 3
+        self.neighbors = []
+        self.choice = None
+        self.earlyAdopter = False
 
     def addNeighbor(self, neighbor):
         self.neighbors.append(neighbor)
@@ -23,29 +26,24 @@ class Agent:
         return self.choice
 
     def updateNeighborPosteriors(self):
+        choice = self.choice
         for neighbor in self.neighbors:
-            neighbor.updatePosteriors()
+            neighbor.updatePosteriors(choice)
 
-    def updatePosteriors(self):
+    def updatePosteriors(self, choice):
         sum = 0
-        postProb= [0]*3
-        for i in range(3):  # i is index for working tap number
-            pos = DiffusionOfInnovations.likelihoods[tapChosen][i]
-            postProb[i] = network.agents[x].priors[i] * pos
+        postProb = [0] * 3
+        for i in range(3):
+            pos = Agent.likelihoods[choice][i]
+            postProb[i] = self.priors[i] * pos
             sum += postProb[i]
         alpha = 1 / sum
         for j in range(3):
-            network.agents[x].priors[j] = postProb[j]*alpha
-
-    def reset(self):
-        self.priors = [1 / 3] * 3
-        self.neighbors = []
-        self.choice = None
-        self.earlyAdopter = False
+            self.priors[j] = postProb[j] * alpha
 
     def __str__(self):
         string = 'Agent id:\n\t' + str(self.id) + '\npriors:\n'
-        string += '\tEarly Adopter: ' + str(self.earlyAdopter)
+        string += '\tEarly Adopter: ' + str(self.earlyAdopter) + '\n'
         priorTotal = 0
         for prior in self.priors:
             string += '\t' + str(prior) + '\n'
--- DiffusionOfInnovations.py
+++ DiffusionOfInnovations.py
@@ -1,31 +1,42 @@
+import random
 import Network
 from Tap import Tap
 
 class DiffusionOfInnovations:
-    def __init__(self):
-        self.reset()
+    def cascadeRuns(self):
+        n = random.randint(1, 3)
+        self.cascadeRun(None, n)
+        self.cascadeRun([1, 0, 0], n)
+        self.cascadeRun([0, 1 / 2, 1 / 2], n)
 
-    def cascadeRun(self, network):
+    def cascadeRun(self, priors, n):
         self.reset()
-        self.randomPriors()
-        ids = list(range(len(network.agents)))
-        random.shuffle(ids)
-        for i in ids:
-            agent = network.agents[i]
-            agent.chooseTap()
+        print()
+        for network in self.networks:
+            ids = list(range(len(network.agents)))
+            random.shuffle(ids)
+            if priors != None:
+                for i in range(n):
+                    network.agents[ids[i]].priors = priors
+            for i in ids:
+                agent = network.agents[i]
+                priors = agent.priors
+                agent.chooseTap()
+                print(str(agent.id) + '-' + str(agent.choice), end=', ')
+            print()
 
     def reset(self):
         self.taps = [0] * 3
         for i in range(len(self.taps)):
             self.taps[i] = Tap(i)
         self.taps[0].isAvailable = True
-
         self.networks = [Network.makeOneCycle(), Network.makeTwoCycle(), Network.makeComplete(), Network.makeAdHoc()]
 
     def __str__(self):
         string = '------------------------------------------ DiffusionOfInnovations ------------------------------------------\n'
-        for network in self.network:
+        for network in self.networks:
             string += str(network) + '\n\n'
         return string
 
-print(DiffusionOfInnovations())
+diffusionOfInnovations = DiffusionOfInnovations()
+diffusionOfInnovations.cascadeRuns()
